f08199710c5bf711d3993b10b10eb33d85f3e94a
Michihiro Kawada <m@llll.jp>
2024-08-07 07:16:45 +0900

    add gamePattern

---
+++ game.js
@@ -0,0 +1,16 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+let randomChosenColour = "";
+let gamePattern = [];
+nextSequence()
+nextSequence()
+nextSequence()
+
+console.log(gamePattern)
+
+
+function nextSequence() {
+  const randomNumber = Math.ceil(Math.random() * 3)
+  randomChosenColour = buttonColours[randomNumber];
+  // console.log(randomNumber)
+  gamePattern.push(randomChosenColour);
+}
