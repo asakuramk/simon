fffe366a5d835fb34a61938dfee5f58dfd4de373
Michihiro Kawada <m@llll.jp>
2024-08-09 21:41:16 +0900

    handler実装

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 let randomChosenColour = "";
 let gamePattern = [];
+let userClickedPattern = [];
 
 
+// nextSequence();
 
-console.log(gamePattern)
-$("#level-title").on("click", function () {
-  nextSequence()
-})
+$('.btn').on("click", function handler() {
+  let userChosenColour = "";
+  userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
+
 
 function nextSequence() {
   const randomNumber = Math.ceil(Math.random() * 3)
